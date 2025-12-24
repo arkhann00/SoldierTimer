98a6fb23dc11322b37e4102367bdac20409a2cf6
arkhann00 <khaarsyan@gmail.com>
2025-12-24 20:33:00 +0300

    Initial Commit

---
+++ SoldierTimer/SoldierTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoldierTimerApp.swift
+//  SoldierTimer
+//
+//  Created by Арсен Хачатрян on 24.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SoldierTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
